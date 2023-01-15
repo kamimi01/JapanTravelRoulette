d322099fbd66919be68b5d9214f0320d42af4630
mika.urakawa <awakaru0122@gmail.com>
2023-01-15 21:19:25 +0900

    first commit

---
+++ JapanTravelRoulette/JapanTravelRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  JapanTravelRouletteApp.swift
+//  JapanTravelRoulette
+//
+//  Created by mikaurakawa on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct JapanTravelRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
