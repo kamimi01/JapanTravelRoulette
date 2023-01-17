743463b6ec21912dc2b24e73877dbe7ab09b0244
mika.urakawa <awakaru0122@gmail.com>
2023-01-17 12:49:10 +0900

    add: navigationbar in roulette screen

--- JapanTravelRoulette/Roulette/RouletteScreen.swift
+++ JapanTravelRoulette/Roulette/RouletteScreen.swift
@@ -11,42 +11,46 @@ struct RouletteScreen: View {
     @ObservedObject var viewModel = RouletteScreenViewModel()
 
     var body: some View {
-        VStack(spacing: 20) {
-            switch viewModel.rouletteStatus {
-            case .notRolling:
-                Text("ボタンを回してね👇")
-                    .font(.title)
+        NavigationView {
+            VStack(spacing: 20) {
+                switch viewModel.rouletteStatus {
+                case .notRolling:
+                    Text("ボタンを押してね👇")
+                        .font(.title)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 100)
+                case .rolling:
+                    LottieView(animationType: .oneTwoThree)
+                        .frame(width: 100, height: 100)
+                case .endRolling:
+                    VStack(alignment: .center, spacing: 5) {
+                        Text(viewModel.selectedPrefecture?.rawValue ?? "なし")
+                            .font(.title)
+                        Text("\nにいこう！")
+                            .font(.title2)
+                    }
                     .frame(maxWidth: .infinity)
                     .frame(height: 100)
-            case .rolling:
-                LottieView(animationType: .oneTwoThree)
-                    .frame(width: 100, height: 100)
-            case .endRolling:
-                VStack(alignment: .center, spacing: 5) {
-                    Text(viewModel.selectedPrefecture?.rawValue ?? "なし")
-                        .font(.title)
-                    Text("\nにいこう！")
-                        .font(.title2)
                 }
-                .frame(maxWidth: .infinity)
-                .frame(height: 100)
+                HStack {
+                    Image("")
+                    Image("")
+                }
+                Button(action: {
+                    viewModel.startRoulette()
+                }) {
+                     Text("旅をする")
+                        .font(.title)
+                        .frame(width: 200, height: 200)
+                        .imageScale(.large)
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                        .shadow(color: .gray, radius: 3, x: 5, y: 5)
+                }
             }
-            HStack {
-                Image("")
-                Image("")
-            }
-            Button(action: {
-                viewModel.startRoulette()
-            }) {
-                 Text("旅をする")
-                    .font(.title)
-                    .frame(width: 200, height: 200)
-                    .imageScale(.large)
-                    .background(Color.orange)
-                    .foregroundColor(.white)
-                    .clipShape(Circle())
-                    .shadow(color: .gray, radius: 3, x: 5, y: 5)
-            }
+            .navigationTitle("次はどこへ行く？")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
